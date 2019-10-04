f488aeb15be302f8384432902e047d104a2b99e3
Kiryl Nazarau <kiryl.nazarau@pegasus.ae>
2019-10-04 13:21:46 +0400

    Mirror test

--- src/main/java/tudelft/chocolate/ChocolateBags.java
+++ src/main/java/tudelft/chocolate/ChocolateBags.java
@@ -13,4 +13,12 @@ public class ChocolateBags {
 
     }
 
-}
+    public int calculate2(int small, int big, int goal) {
+        int maxBigBoxes = goal / 5;
+        int bigBoxesWeCanUse = maxBigBoxes < big ? maxBigBoxes : big;
+        goal -= (bigBoxesWeCanUse * 5);
+        if (small < goal) {
+            return -1;
+        }
+        return goal;
+    }}
--- src/test/java/tudelft/mirror/MirrorTest.java
+++ src/test/java/tudelft/mirror/MirrorTest.java
@@ -1,4 +1,16 @@
 package tudelft.mirror;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class MirrorTest {
+
+  @ParameterizedTest
+  @CsvSource({ "abba, abba", "abca, a", "aa, aa", "e, e", "eeeteee, eeeteee", "ttggtt, ttggtt"})
+  public void totalIsTooBig(String text, String expectedResult) {
+    Mirror mirror = new Mirror();
+    String result = mirror.mirrorEnds(text);
+    Assertions.assertEquals(expectedResult, result);
+  }
 }
