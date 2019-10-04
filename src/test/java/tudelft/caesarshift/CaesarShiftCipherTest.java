5bed2028458f7fac16a76ed8bb48e947d0f2737c
Kiryl Nazarau <kiryl.nazarau@pegasus.ae>
2019-10-04 14:36:39 +0400

    CaesarShiftCipherTest2

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 public class CaesarShiftCipherTest {
 
   @ParameterizedTest
-  @CsvSource({"ab, 1, bc", "xyz, 3, abc", "d, -2, b", "aaa bbb ccc ddd, 1, bbb ccc ddd eee", "f, 0, f"})
+  @CsvSource({"ab, 1, bc", "xyz, 3, abc", "d, -2, b", "aaa bbb ccc ddd, 1, bbb ccc ddd eee", "f, 0, f", " F, 5, invalid"})
   public void caesarShiftTest(String message, int shift, String expectedResult) {
     CaesarShiftCipher shiftCipher = new CaesarShiftCipher();
     String result = shiftCipher.caesarShiftCipher(message, shift);
