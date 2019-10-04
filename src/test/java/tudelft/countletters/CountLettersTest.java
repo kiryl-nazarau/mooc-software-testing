02d129ff11e20fb1ce3f675ff547ebef2bc170a6
Kiryl Nazarau <kiryl.nazarau@pegasus.ae>
2019-10-04 15:30:58 +0400

    CountLetterTest

--- src/test/java/tudelft/countletters/CountLettersTest.java
+++ src/test/java/tudelft/countletters/CountLettersTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 public class CountLettersTest {
 
     @Test
-    public void multipleMatchingWords() {
+    public void multipleSMatchingWords() {
         int words = new CountLetters().count("cats|dogs");
         Assertions.assertEquals(2, words);
     }
@@ -16,5 +16,27 @@ public class CountLettersTest {
         int words = new CountLetters().count("cats|dog");
         Assertions.assertEquals(1, words);
     }
+
+    @Test
+    public void oneWordWithR() {
+        int words = new CountLetters().count("car");
+        Assertions.assertEquals(1, words);
+    }
+
+    @Test
+    public void multipleRMatchingWords() {
+        int words = new CountLetters().count("car|dogs");
+        Assertions.assertEquals(2, words);
+    }
+
+    @Test
+    public void noWords() {
+        int words = new CountLetters().count("sun!noon");
+        Assertions.assertEquals(0, words);
+    }
+
+
+
+
 
 }
